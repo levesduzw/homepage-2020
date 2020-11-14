81613a01e74006451e4645c03ffdee851043d1f3
Levente Sermaul <levente.sermaul@ericsson.com>
2020-11-14 03:01:16 +0100

    Changed bookmarks.js to contain a stringified json (workaround). The plan is for a script to generate the json string from e.g. a bookmarks file, Raindrop.io.

--- bookmarks.js
+++ bookmarks.js
@@ -1,129 +1,233 @@
-// Note: having length != 4 will mess with layout based on how the site is styled
-const bookmarks = [
+const bookmarks_json = `
+[
   {
-    title: "Daily",
-    links: [
-      { name: "Inbox", url: "https://inbox.google.com" },
-      { name: "GitHub", url: "https://github.com" },
-      { name: "Drive", url: "https://drive.google.com" },
-    ],
-  },
-  {
-    title: "Media",
-    links: [
-      { name: "Youtube", url: "https://youtube.com" },
-      { name: "Netflix", url: "https://netflix.com" },
-      { name: "Crunchyroll", url: "https://crunchyroll.com" },
+    "title": "Daily",
+    "links": [
       {
-        name: "Amazon Prime",
-        url: "https://www.amazon.com/Amazon-Video",
+        "name": "Inbox",
+        "url": "https://inbox.google.com"
       },
-    ],
-  },
-  {
-    title: "Reddit",
-    links: [
-      { name: "/r/overwatch", url: "https://reddit.com/r/overwatch" },
       {
-        name: "/r/pcmasterrace",
-        url: "https://reddit.com/r/pcmasterrace",
+        "name": "GitHub",
+        "url": "https://github.com"
       },
-      { name: "/r/me_irl", url: "https://reddit.com/r/me_irl" },
       {
-        name: "/r/battlestations",
-        url: "https://reddit.com/r/battlestations",
-      },
-      { name: "/r/unixporn", url: "https://reddit.com/r/unixporn" },
-      { name: "/r/news", url: "https://reddit.com/r/news" },
-    ],
+        "name": "Drive",
+        "url": "https://drive.google.com"
+      }
+    ]
   },
   {
-    title: "Social",
-    links: [
-      { name: "Twitter", url: "https://twitter.com" },
-      { name: "Facebook", url: "https://facebook.com" },
-    ],
-  },
-  {
-    title: "Daily",
-    links: [
-      { name: "Inbox", url: "https://inbox.google.com" },
-      { name: "GitHub", url: "https://github.com" },
-      { name: "Drive", url: "https://drive.google.com" },
-    ],
-  },
-  {
-    title: "Media",
-    links: [
-      { name: "Youtube", url: "https://youtube.com" },
-      { name: "Netflix", url: "https://netflix.com" },
-      { name: "Crunchyroll", url: "https://crunchyroll.com" },
+    "title": "Media",
+    "links": [
       {
-        name: "Amazon Prime",
-        url: "https://www.amazon.com/Amazon-Video",
+        "name": "Youtube",
+        "url": "https://youtube.com"
       },
-    ],
-  },
-  {
-    title: "Reddit",
-    links: [
-      { name: "/r/overwatch", url: "https://reddit.com/r/overwatch" },
       {
-        name: "/r/pcmasterrace",
-        url: "https://reddit.com/r/pcmasterrace",
+        "name": "Netflix",
+        "url": "https://netflix.com"
       },
-      { name: "/r/me_irl", url: "https://reddit.com/r/me_irl" },
       {
-        name: "/r/battlestations",
-        url: "https://reddit.com/r/battlestations",
+        "name": "Crunchyroll",
+        "url": "https://crunchyroll.com"
       },
-      { name: "/r/unixporn", url: "https://reddit.com/r/unixporn" },
-      { name: "/r/news", url: "https://reddit.com/r/news" },
-    ],
-  },
-  {
-    title: "Social",
-    links: [
-      { name: "Twitter", url: "https://twitter.com" },
-      { name: "Facebook", url: "https://facebook.com" },
-    ],
-  },
-  {
-    title: "Reddit",
-    links: [
-      { name: "/r/overwatch", url: "https://reddit.com/r/overwatch" },
       {
-        name: "/r/pcmasterrace",
-        url: "https://reddit.com/r/pcmasterrace",
-      },
-      { name: "/r/me_irl", url: "https://reddit.com/r/me_irl" },
+        "name": "Amazon Prime",
+        "url": "https://www.amazon.com/Amazon-Video"
+      }
+    ]
+  },
+  {
+    "title": "Reddit",
+    "links": [
       {
-        name: "/r/battlestations",
-        url: "https://reddit.com/r/battlestations",
+        "name": "/r/overwatch",
+        "url": "https://reddit.com/r/overwatch"
       },
-      { name: "/r/unixporn", url: "https://reddit.com/r/unixporn" },
-      { name: "/r/news", url: "https://reddit.com/r/news" },
-    ],
+      {
+        "name": "/r/pcmasterrace",
+        "url": "https://reddit.com/r/pcmasterrace"
+      },
+      {
+        "name": "/r/me_irl",
+        "url": "https://reddit.com/r/me_irl"
+      },
+      {
+        "name": "/r/battlestations",
+        "url": "https://reddit.com/r/battlestations"
+      },
+      {
+        "name": "/r/unixporn",
+        "url": "https://reddit.com/r/unixporn"
+      },
+      {
+        "name": "/r/news",
+        "url": "https://reddit.com/r/news"
+      }
+    ]
   },
   {
-    title: "Social",
-    links: [
-      { name: "Twitter", url: "https://twitter.com" },
-      { name: "Facebook", url: "https://facebook.com" },
-    ],
+    "title": "Social",
+    "links": [
+      {
+        "name": "Twitter",
+        "url": "https://twitter.com"
+      },
+      {
+        "name": "Facebook",
+        "url": "https://facebook.com"
+      }
+    ]
   },
   {
-    title: "Social",
-    links: [
-      { name: "Twitter", url: "https://twitter.com" },
-      { name: "Facebook", url: "https://facebook.com" },
-    ],
+    "title": "Daily",
+    "links": [
+      {
+        "name": "Inbox",
+        "url": "https://inbox.google.com"
+      },
+      {
+        "name": "GitHub",
+        "url": "https://github.com"
+      },
+      {
+        "name": "Drive",
+        "url": "https://drive.google.com"
+      }
+    ]
   },
   {
-    title: "Social",
-    links: [
-      { name: "Twitter", url: "https://twitter.com" },
-      { name: "Facebook", url: "https://facebook.com" },
-    ],
+    "title": "Media",
+    "links": [
+      {
+        "name": "Youtube",
+        "url": "https://youtube.com"
+      },
+      {
+        "name": "Netflix",
+        "url": "https://netflix.com"
+      },
+      {
+        "name": "Crunchyroll",
+        "url": "https://crunchyroll.com"
+      },
+      {
+        "name": "Amazon Prime",
+        "url": "https://www.amazon.com/Amazon-Video"
+      }
+    ]
+  },
+  {
+    "title": "Reddit",
+    "links": [
+      {
+        "name": "/r/overwatch",
+        "url": "https://reddit.com/r/overwatch"
+      },
+      {
+        "name": "/r/pcmasterrace",
+        "url": "https://reddit.com/r/pcmasterrace"
+      },
+      {
+        "name": "/r/me_irl",
+        "url": "https://reddit.com/r/me_irl"
+      },
+      {
+        "name": "/r/battlestations",
+        "url": "https://reddit.com/r/battlestations"
+      },
+      {
+        "name": "/r/unixporn",
+        "url": "https://reddit.com/r/unixporn"
+      },
+      {
+        "name": "/r/news",
+        "url": "https://reddit.com/r/news"
+      }
+    ]
+  },
+  {
+    "title": "Social",
+    "links": [
+      {
+        "name": "Twitter",
+        "url": "https://twitter.com"
+      },
+      {
+        "name": "Facebook",
+        "url": "https://facebook.com"
+      }
+    ]
+  },
+  {
+    "title": "Reddit",
+    "links": [
+      {
+        "name": "/r/overwatch",
+        "url": "https://reddit.com/r/overwatch"
+      },
+      {
+        "name": "/r/pcmasterrace",
+        "url": "https://reddit.com/r/pcmasterrace"
+      },
+      {
+        "name": "/r/me_irl",
+        "url": "https://reddit.com/r/me_irl"
+      },
+      {
+        "name": "/r/battlestations",
+        "url": "https://reddit.com/r/battlestations"
+      },
+      {
+        "name": "/r/unixporn",
+        "url": "https://reddit.com/r/unixporn"
+      },
+      {
+        "name": "/r/news",
+        "url": "https://reddit.com/r/news"
+      }
+    ]
+  },
+  {
+    "title": "Social",
+    "links": [
+      {
+        "name": "Twitter",
+        "url": "https://twitter.com"
+      },
+      {
+        "name": "Facebook",
+        "url": "https://facebook.com"
+      }
+    ]
+  },
+  {
+    "title": "Social",
+    "links": [
+      {
+        "name": "Twitter",
+        "url": "https://twitter.com"
+      },
+      {
+        "name": "Facebook",
+        "url": "https://facebook.com"
+      }
+    ]
+  },
+  {
+    "title": "Social",
+    "links": [
+      {
+        "name": "Twitter",
+        "url": "https://twitter.com"
+      },
+      {
+        "name": "Facebook",
+        "url": "https://facebook.com"
+      }
+    ]
   }
-];
+]
+`;
+const bookmarks = JSON.parse(bookmarks_json);
