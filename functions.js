f93d226e1774a7c2997ededa20a0bacc4c8d7ec0
Levente Sermaul <levente.sermaul@ericsson.com>
2024-05-03 20:00:04 +0200

    Function updates:
    bookmark-title-container for new Open all icon
    random wallpaper function
    multi-search urls (todo)
    key instead of keycode

--- functions.js
+++ functions.js
@@ -1,27 +1,55 @@
+
+
+const wallpaperPath = '/home/esrmlvn/Pictures/Wallpapers/';
+const wallpaperList = [
+    "0lOe7Ei.png",
+    "2veek29rath51.jpg",
+    "beach_sea_waves_145932_3840x2160.jpg",
+    "DI3Oxg8.jpg",
+    "o6CEpAb.jpg",
+    "PVfXvuI.png",
+    "wallpapersden.com_minimalist-black-digital-blend_2560x1440.jpg"
+];
+
 const searchUrl = "https://google.com/search?q=";
+// const dSearchUrl = "https://duckduckgo.com/?q="
+// const bSearchUrl = "https://www.bing.com/search?q="
+// const nSearchUrl = "https://search.naver.com/search.naver?query="
+// const ySearchUrl = "https://www.youtube.com/results?search_query=""
+
+const bookmarkContainer = document.getElementById("bookmark-container");
+
 // Search on enter key event
 function search(e) {
-if (e.keyCode == 13) {
-    var val = document.getElementById("search-field").value;
-    window.open(searchUrl + val);
+    if (e.key == 'Enter') {
+        var val = document.getElementById("search-field").value;
+        window.open(searchUrl + val);
+        // TODO
+        // Open multiple tabs from JavaScript
+        // https://gist.github.com/zacharysyoung/d8498aecabd42ffbe3778522fbf4b25a
+        // const anchors = document.getElementsByTagName('a');
+        // console.log(anchors)
+        // for (const x of anchors) {
+        //     console.log(x);
+        //     window.open(x.href, '_blank');    // enable popups + be careful
+        // }
+    }
 }
-}
+
 // Get current time and format
 function getTime() {
-let date = new Date(),
-    min = date.getMinutes(),
-    sec = date.getSeconds(),
-    hour = date.getHours();
+    let date = new Date(),
+        min = date.getMinutes(),
+        sec = date.getSeconds(),
+        hour = date.getHours();
 
-return (
-    "" +
-    (hour < 10 ? "0" + hour : hour) +
-    ":" +
-    (min < 10 ? "0" + min : min) +
-    ":" +
-    (sec < 10 ? "0" + sec : sec)
-);
+    return (
+        "" + (hour < 10 ? "0" + hour : hour) +
+        ":" + (min < 10 ? "0" + min : min) +
+        ":" + (sec < 10 ? "0" + sec : sec)
+    );
 }
+
 // Handle Weather request
 function getWeather() {
 let xhr = new XMLHttpRequest();
@@ -45,27 +73,38 @@ xhr.send();
 }
 // Handle writing out Bookmarks
 function setupBookmarks() {
-const bookmarkContainer = document.getElementById("bookmark-container");
-bookmarkContainer.innerHTML = bookmarks
-    .map((b) => {
-    const html = ["<div class='bookmark-set'>"];
-    html.push(`<div class="bookmark-title">${b.title}</div>`);
-    html.push('<div class="bookmark-inner-container">');
-    html.push(
-        ...b.links.map(
-        (l) =>
-            `<a class="bookmark" href="${l.url}">${l.name}</a>`
-        )
-    );
-    html.push("</div></div>");
-    return html.join("");
-    })
-    .join("");
+    const bookmarkContainer = document.getElementById("bookmark-container");
+    bookmarkContainer.innerHTML = bookmarks
+        .map((b) => {
+        const html = ["<div class='bookmark-set'>"];
+        html.push(`<div class="bookmark-title-container">`);
+        html.push(`<div class="bookmark-title">${b.title}</div>`);
+        html.push(`<a class="bookmark-open-all" href="test"><img src="external-link-line.svg"></img></a>`);
+        html.push(`</div>`);
+        html.push('<div class="bookmark-inner-container">');
+        html.push(
+            ...b.links.map(
+            (l) =>
+                `<a class="bookmark" href="${l.url}">${l.name}</a>`
+            )
+        );
+        html.push("</div></div>");
+        return html.join("");
+        })
+        .join("");
+}
+
+// Set random wallpaper on page load
+function setRandomWallpaper() {
+    randomWallpaperPath = wallpaperPath + wallpaperList[Math.floor(Math.random() * wallpaperList.length)];
+    console.log(randomWallpaperPath);
+    document.getElementById("container").style.backgroundImage = "url('" + randomWallpaperPath + "')";
 }
 
 window.onload = () => {
 setupBookmarks();
 getWeather();
+// setRandomWallpaper();
 // Set up the clock
 document.getElementById("clock").innerHTML = getTime();
 // Set clock interval to tick clock
@@ -75,11 +114,11 @@ setInterval(() => {
 };
 
 document.addEventListener("keyup", (event) => {
-if (event.keyCode == 32) {
+if (event.key == ' ') {
     // Spacebar code to open search
     document.getElementById("search").style.display = "flex";
     document.getElementById("search-field").focus();
-} else if (event.keyCode == 27) {
+} else if (event.key == 'Escape') {
     // Esc to close search
     document.getElementById("search-field").value = "";
     document.getElementById("search-field").blur();
